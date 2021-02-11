4da9c6206ecaa6cac578e386b1a47ac3ca7422aa
unknown <kauansantos83@gmail.com>
2021-02-10 21:43:14 -0300

    commit1

---
+++ src/components/partials/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FooterArea } from './styled';
+
+const Footer = () => {
+    return (
+        <FooterArea>
+           <br/>
+            
+        </FooterArea>
+    );
+}
+
+export default Footer;
